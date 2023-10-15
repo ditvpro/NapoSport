07df7433df4ec0cc02dd4a4e900eaad4cdb268b7
ditvpro <64935338+ditvpro@users.noreply.github.com>
2023-10-15 21:32:53 +0700

    Create and Action View Component

--- NapoSport/Areas/Customer/Controllers/HomeController.cs
+++ NapoSport/Areas/Customer/Controllers/HomeController.cs
@@ -22,14 +22,6 @@ namespace NapoSport.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
-            var claimsIndetity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIndetity.FindFirst(ClaimTypes.NameIdentifier);
-
-            if (claim != null)
-            {
-                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
-            }
-
             IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category,Brand");
             return View(products);
         }
---
+++ NapoSport/Areas/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NapoSport.DataAccess.Repository.IRepository;
+using NapoSport.Utility;
+using System.Security.Claims;
+
+namespace NapoSport.Areas.ViewComponents
+{
+    public class ShoppingCartViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIndetity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIndetity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(claim != null)
+            {
+                if(HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+                }
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
