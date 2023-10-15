d4e904b3587a8e69fa9fc5367ee6455de60d125b
ditvpro <64935338+ditvpro@users.noreply.github.com>
2023-10-15 23:44:27 +0700

    Reversion .net 7

--- NapoSport/Areas/Admin/Controllers/OrderController.cs
+++ NapoSport/Areas/Admin/Controllers/OrderController.cs
@@ -85,7 +85,7 @@ namespace NapoSport.Areas.Admin.Controllers
 
             if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
             {
-                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+                orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
             }
 
             _unitOfWork.OrderHeader.Update(orderHeader);
